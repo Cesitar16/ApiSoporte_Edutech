649e2174776a6859e978ef61ec0398b13fd2f2a7
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-07-02 22:40:08 -0400

    Aplicado Metodo de HATEOAS

--- src/main/java/com/edutech/soporte/controllers/SoporteController.java
+++ src/main/java/com/edutech/soporte/controllers/SoporteController.java
@@ -3,6 +3,7 @@ package com.edutech.soporte.controllers;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -69,5 +70,46 @@ public class SoporteController {
         } catch (RuntimeException ex) {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/hateoas/{id}")
+    public SoporteResponseDTO obtenerHATEOAS(@PathVariable Integer id) {
+        SoporteResponseDTO dto = soporteService.buscarSoportePorId(id);
+
+        String gatewayUrl = "http://localhost:8888/api/proxy/soportes";
+
+        // Link a sí mismo
+        dto.add(Link.of(gatewayUrl + "/hateoas/" + id).withSelfRel());
+
+        // Link a la lista de todos los soportes
+        dto.add(Link.of(gatewayUrl + "/hateoas").withRel("todos-los-soportes"));
+
+        // Link para eliminar
+        dto.add(Link.of(gatewayUrl + "/" + id).withRel("eliminar").withType("DELETE"));
+
+        // Link para actualizar
+        dto.add(Link.of(gatewayUrl + "/" + id).withRel("actualizar").withType("PUT"));
+
+        return dto;
+    }
+
+    /**
+     * Obtiene todos los tickets de soporte y añade enlaces HATEOAS a cada uno.
+     */
+    @GetMapping("/hateoas")
+    public List<SoporteResponseDTO> listarHATEOAS() {
+        List<SoporteResponseDTO> soportes = soporteService.listarSoportes();
+        String gatewayUrl = "http://localhost:8888/api/proxy/soportes";
+
+        for (SoporteResponseDTO dto : soportes) {
+
+            dto.add(Link.of(gatewayUrl + "/hateoas/" + dto.getId()).withSelfRel());
+
+            dto.add(Link.of(gatewayUrl).withRel("editar-soporte").withType("PUT"));
+
+            dto.add(Link.of(gatewayUrl).withRel("eliminar-soporte").withType("DELETE"));
+        }
+
+        return soportes;
     }
 }
--- src/main/java/com/edutech/soporte/dto/SoporteResponseDTO.java
+++ src/main/java/com/edutech/soporte/dto/SoporteResponseDTO.java
@@ -4,11 +4,12 @@ import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class SoporteResponseDTO {
+public class SoporteResponseDTO extends RepresentationModel<SoporteResponseDTO> {
     private Integer id;
     private String asunto;
     private String descripcion;
